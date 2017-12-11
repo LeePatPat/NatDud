3bc2502c72d050518115aaf6cdeb5c67496479d6
Lee <lee.paterson.2014@uni.strath.ac.uk>
2017-12-11 02:06:09 +0000

    Updated JQuery version

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 	<!-- Custom NatDud CSS -->
 	<link href="css/NatDud.css" rel="stylesheet">
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- jQuery UI (used for animation and manipulation of UI) -->
 	<script src="http://code.jquery.com/ui/1.9.1/jquery-ui.js" type="text/javascript"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
