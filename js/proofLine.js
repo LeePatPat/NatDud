36309166c10888636fd22de31b0942c62522bd1a
Lee <lee.paterson.2014@uni.strath.ac.uk>
2018-01-09 02:48:46 +0000

    Added ProofLine to its own class for better modularisation

--- js/proofLine.js
+++ js/proofLine.js
@@ -1,11 +1,11 @@
 /** A class to represent proof lines as a more logical data structure */
 class ProofLine {
     /**
-     * @param {string} dependencies     - assumption dependencies
-     * @param {string} lineNum           - line number
-     * @param {string} proposition       - proposition result from assumption or inference rule
-     * @param {string} rule              - rule used (or assumption)
-     * @param {string} ruleDependencies - line numbers that given inference rule depends on
+     * @param {Array.number} dependencies       - assumption dependencies
+     * @param {string} lineNum                  - line number
+     * @param {string} proposition              - proposition result from assumption or inference rule
+     * @param {string} rule                     - rule used (or assumption)
+     * @param {Array.numebr} ruleDependencies   - line numbers that given inference rule depends on
      */
     constructor(dependencies, lineNum, proposition, rule, ruleDependencies) {
         this.dependencies = dependencies;
@@ -22,19 +22,12 @@ class ProofLine {
     getRule() { return this.rule; }
     getRuleDependencies() { return this.ruleDependencies; }
 
-    //setters
-    setDependencies(dep) { this.dependencies = dep; }
-    setLineNum(l) { this.lineNum = l; }
-    setProposition(prop) { this.proposition = prop; }
-    setRule(r) { this.rule = r; }
-    setRuleDependencies(rDep) { this.ruleDependencies = rDep; }
-
     /**
      * @return {string} - proof line as a string 
      */
     getLineAsString() {
         return this.dependencies + " " +
-            "(" + lineNum.toString() + ")" + " " +
+            "(" + this.lineNum.toString() + ")" + " " +
             this.proposition + " " +
             this.rule + " " +
             this.ruleDependencies;
