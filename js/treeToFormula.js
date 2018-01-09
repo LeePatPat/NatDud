4cc96e50f5963f503b21e8db0dd335d08d9b947f
Lee <lee.paterson.2014@uni.strath.ac.uk>
2018-01-09 02:57:14 +0000

    Added treeToFormula function to its own file for modularity

---
+++ js/treeToFormula.js
@@ -0,0 +1,19 @@
+export function treeToFormula(formulaTree, operandNo){
+	//base cases
+	if(!("children" in formulaTree)){ //if a literal
+		return formulaTree["name"];
+	}else if(formulaTree["name"] === "~"){ //only 1 child but is an operator (~)
+		return "~" + treeToFormula(formulaTree["children"][0], operandNo);
+	}
+	
+	operandNo++;
+	
+	//index 1 is left most child in tree
+	var result = treeToFormula(formulaTree["children"][1], operandNo) 
+				+ formulaTree["name"]
+				+ treeToFormula(formulaTree["children"][0], operandNo);
+		
+	if(operandNo === 1) //this ensures that no redundant surrounding brackets occur
+		return result;
+	return "(" + result + ")";
+}
