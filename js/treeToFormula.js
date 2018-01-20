0b5c7b45319df2be0e61992c43847a3bf4d6a7d1
Lee <leepaterson1996@hotmail.com>
2018-01-20 22:14:05 +0000

    made conditions slightly more readable

--- js/treeToFormula.js
+++ js/treeToFormula.js
@@ -3,18 +3,25 @@ function treeToFormula(formulaTree, operandNo){
 	//base cases
 	if(!("children" in formulaTree)) //if a literal
 		return formulaTree["name"];
-	else if(formulaTree["name"] === "~" && (formulaTree["children"][0]["name"] === "->" || formulaTree["children"][0]["name"] === "&" || formulaTree["children"][0]["name"] === "||" ) ) //child is operator
+	
+	else if(formulaTree["name"] === "~" 
+				&& (formulaTree["children"][0]["name"] === "->" 
+						|| formulaTree["children"][0]["name"] === "&" 
+						|| formulaTree["children"][0]["name"] === "||" ) ) //child is operator
 		return "~" + "(" + treeToFormula(formulaTree["children"][0], operandNo) + ")";
+
 	else if(formulaTree["name"] === "~" && formulaTree["children"].length === 1) //child is ~ or literal
 		return "~" + treeToFormula(formulaTree["children"][0], operandNo);
 		
 	operandNo++;
 	
 	//index 1 is left most child in tree
-	var result = treeToFormula(formulaTree["children"][1], operandNo) 
+	var result =  treeToFormula(formulaTree["children"][1], operandNo) 
 				+ formulaTree["name"]
 				+ treeToFormula(formulaTree["children"][0], operandNo);
-		
+	
+	console.log(operandNo);
+
 	if(operandNo === 1) //this ensures that no redundant surrounding brackets occur
 		return result;
 	return "(" + result + ")";
